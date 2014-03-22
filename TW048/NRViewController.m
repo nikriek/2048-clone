cda3c42b4d61e1f8d9cf1c1998e9823c6459cd8b
RexUmbrarum <20england11@gmail.com>
2014-03-22 02:55:33 +0100

    Added some funky background music plus the SKView contains additional debugging information

--- TW048/NRViewController.m
+++ TW048/NRViewController.m
@@ -9,6 +9,12 @@
 #import "NRViewController.h"
 #import "NRMyScene.h"
 
+@import AVFoundation;
+
+@interface NRViewController ()
+@property (nonatomic) AVAudioPlayer *backgroundMusicPlayer;
+@end
+
 @implementation NRViewController
 
 - (void)viewDidLoad
@@ -19,10 +25,27 @@
     SKView * skView = (SKView *)self.view;
     skView.showsFPS = YES;
     skView.showsNodeCount = YES;
+    skView.showsDrawCount = YES;
+    skView.showsPhysics = YES;
+
     
     // Create and configure the scene.
     SKScene * scene = [NRMyScene sceneWithSize:skView.bounds.size];
     scene.scaleMode = SKSceneScaleModeAspectFill;
+    
+    // Play some lovely background music
+    NSError *error;
+    NSURL *backgroundMusicURL =[[NSBundle mainBundle] URLForResource:@"The Binary Code Song" withExtension:@"mp3"];
+    self.backgroundMusicPlayer = [[AVAudioPlayer alloc] initWithContentsOfURL:backgroundMusicURL error:&error];
+    self.backgroundMusicPlayer.numberOfLoops = -1;
+    self.backgroundMusicPlayer.volume = 0.2f;
+    [self.backgroundMusicPlayer prepareToPlay];
+    [self.backgroundMusicPlayer play];
+    
+    if ( error )
+    {
+        NSLog(@"Error: %@", error.localizedDescription);
+    }
     
     // Present the scene.
     [skView presentScene:scene];
